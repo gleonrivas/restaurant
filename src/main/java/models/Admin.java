65002335b4a2f8ca102d3c99705905a6ba2ddfbf
Gonzalo <gleonrivas@safareyes.es>
2022-04-06 13:25:15 +0200

    deleted and replaced by Person class

--- src/main/java/models/Admin.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-public class Admin extends Person{
-
-    public Admin(int id, String name, String surname, String address, int phone, String dni) {
-        super(id, name, surname, address, phone, dni);
-    }
-
-}
